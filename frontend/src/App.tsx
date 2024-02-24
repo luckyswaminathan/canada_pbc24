60454692751ba59c859516b5b04987c480d82038
luckyswaminathan <lakshman.swaminathan1@gmail.com>
2024-02-23 20:00:30 -0500

    m

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,18 @@
-// src/App.js
-import React, { useState } from 'react';
-import './App.css'; // Import the CSS file
-
-const MyForm = () => {
-  const [message, setMessage] = useState('');
-
-  const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setMessage(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    console.log('Message submitted:', message);
-  };
-
-  return (
-    <div className="my-form-container"> {/* Apply a class for styling */}
-      <h2>What would you like to send today?</h2>
-      <label>
-        Enter here:
-        <input type="text" value={message} onChange={handleInputChange} />
-      </label>
-      <button onClick={handleSubmit}>Submit to Chain</button>
-    </div>
-  );
-};
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
+import Ballot from './pages/ballot';
+import NotFoundPage from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <MyForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ballot" element={<Ballot />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
